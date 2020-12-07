30d310e4dbbd4f76392a75a5679483cbc9dc258e
MattEBoy <teo1997@gmail.com>
2020-12-07 14:28:58 -0700

    log in page working

--- listings/admin.py
+++ listings/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from .models import Minority_Type, Education, User, Skill, User_Skill, Employer_Size, Employer, Job_Type, Job_Listing, Job_Skill, External_Application_Rating, Application
+from .models import Minority_Type, Education, Applicant, Skill, Applicant_Skill, Employer_Size, Employer, Job_Type, Job_Listing, Job_Skill, External_Application_Rating, Application
 
 # Register your models here.
 admin.site.register(Minority_Type)
 admin.site.register(Education)
-admin.site.register(User)
+admin.site.register(Applicant)
 admin.site.register(Skill)
-admin.site.register(User_Skill)
+admin.site.register(Applicant_Skill)
 admin.site.register(Employer_Size)
 admin.site.register(Employer)
 admin.site.register(Job_Type)
